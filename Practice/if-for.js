b44ae561822e696e6a45473e827dc73685dbe077
Honoka_Ishikawa <honoka_ishikawa@force-corp.co.jp>
2021-05-12 17:33:09 +0900

    sixth

--- Practice/if-for.js
+++ Practice/if-for.js
@@ -9,10 +9,29 @@
 // ===の場合、左右を比較して、データ型が違う時
 // 等しくないと判断される（==より厳密にチェックされる）
 
-let testscore = 60;
-
-if(testscore >= 80)
-{document.write("合格です！おめでとうございます！");
+function scoreCheck(){
+    let testscore = document.getElementById('score').value
+    if(testscore >= 80)
+{document.write("合格です！おめでとうございます！"); 
 }else if(testscore >= 70)
 {document.write("惜しい！あと少し！");}
-    else{document.write("不合格です！もうすこし勉強しよう！");}
+    else{document.write("不合格です！もうすこし勉強しよう！");}}
+
+
+
+//     // []・・・「配列」このカッコの中に単語を入れカンマで区切るとデータベースのレコードのように扱える
+
+// const nameList = ["田中","山田","林","佐藤"];
+
+// for (let i = 0; i < nameList.length; i++)
+// {console.log(`${i+1}. ${nameList[i]}`);}
+
+// continue文
+for(let i = 0; i < 10; i++)
+    {if (i % 2 === 0) continue;
+    console.log(i);} 
+
+// break文
+for(let i =1; i < 1000; i++)
+{if(i > 10)break;
+console.log(i);}
